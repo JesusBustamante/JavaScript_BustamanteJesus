052151bb79fba06e3bb39273a18559779dd987c3
JesusBustamante <jesusgustamante08@gmail.com>
2024-08-19 18:16:18 -0500

    feat:  :sparkles: implementation of the three types of variables

--- Dia1/ejercicio1.js
+++ Dia1/ejercicio1.js
@@ -57,3 +57,70 @@ function resta(num1, num2) {
 console.log(resta())
 
 // Funcion sin parametros con retorno
+function division() {
+    dividendo = 10
+    divisor = 5
+
+    resultado = dividendo/divisor
+
+    return resultado
+}
+
+console.log(division())
+
+//--------------------------------------------------------------------
+
+// Variable var 
+var tester = "hey, hola";
+    
+function nuevaFuncion() {
+    var hola = "hola";
+    // Mientras que hola tiene un ámbito local. Así que no podemos 
+    //acceder a la variable hola fuera de la función.
+    
+}
+console.log(tester); // tester tiene un ámbito global porque existe fuera de la función.
+
+//---------------------------------------------------------------------
+
+// Variable const
+
+// const no puede modificarse ni volver a declararse dentro de su mismo ámbito.
+
+//Mientras que un objeto const no se puede actualizar, las propiedades de este objeto sí se pueden actualizar. 
+//Como resultado, si declaramos un objeto const como este:
+const saludar = {
+    mensaje: "dice Hola",
+    tiempos: 4
+}
+
+// Mientras que no podemos hacer esto:
+// saludar = {
+//     palabras: "Hola",
+//     numero: "cinco"
+// }
+
+// Podemos hacer esto:
+saludar.mensaje = "Hola bro";
+console.log(saludar.mensaje)
+
+
+// Variable let 
+// Al igual que var,  una variable declarada con let puede ser actualizada 
+//dentro de su ámbito. A diferencia de var, una variable let no puede ser 
+//re-declarada dentro de su ámbito. 
+
+let saludo = "dice Hola";
+saludo = "dice Hola tambien";
+
+console.log(saludo) // Esto funciona
+
+// let saludar = "dice Hola";
+// let saludar = "dice Hola tambien"; // Esto devolverá un error 
+
+
+
+
+
+
+
