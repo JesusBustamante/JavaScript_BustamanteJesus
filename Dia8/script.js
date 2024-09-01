2c007344b96aded84a01e46cfe2bbc20eea85b44
JesusBustamante <jesusgustamante08@gmail.com>
2024-08-31 22:17:48 -0500

    feat: sparkles: Sub-queries and their printout

--- Dia8/script.js
+++ Dia8/script.js
@@ -6,13 +6,20 @@ function GetDataFromAPI() {
     readid(url)
 }
 
+function subconsult() {
+    var idhero = document.getElementById(`entrada`).value;
+    var urlm = `https://swapi.py4e.com/api/planets/`+idhero
+    return urlm
+}
+
 function readid(url) {
     fetch(url)
     .then (res => res.json())
     .then (data => {
+
         document.getElementById(`output`).innerHTML = ""
         document.getElementById(`output`).innerHTML += `
-
+            
             <table class="table table-dark table-sm">
             <tbody>
                 <tr>
@@ -49,12 +56,107 @@ function readid(url) {
                 </tr>
                 <tr>
                 <td scope="row">Homeworld</th>
-                <td colspan="2">${data.homeworld}</td>
+                <td colspan="2" id="homeworld" style = "background-color: white; color: black;"></td>
+                </tr>
+                <tr>
+                <td scope="row">Films</th>
+                <td colspan="2" id="films" style = "background-color: white; color: black;"></td>
+                </tr>
+                <tr>
+                <td scope="row">Species</th>
+                <td colspan="2" id="species" style = "background-color: white; color: black;"></td>
+                </tr>
+                <tr>
+                <td scope="row">Vehicles</th>
+                <td colspan="2" id="vehicles" style = "background-color: white; color: black;"></td>
                 </tr>
             </tbody>
             </table>
-
- 
         `;
+
+        fetch(data.homeworld)
+        .then(res => res.json())
+        .then(homeworldData => {
+            document.getElementById(`homeworld`).innerHTML = ""
+            document.getElementById(`homeworld`).innerHTML += `
+        
+                <p>Name: ${homeworldData.name}</p>
+                <p>Rotation period: ${homeworldData.rotation_period}</p>
+                <p>Orbital period: ${homeworldData.orbital_period}</p>
+                <p>Diameter: ${homeworldData.diameter}</p>
+                <p>Climate: ${homeworldData.climate}</p>
+                <p>Gravity: ${homeworldData.gravity}</p>
+                <p>Terrain: ${homeworldData.terrain}</p>
+                <p>Surface water: ${homeworldData.surface_water}</p>
+                <p>Population: ${homeworldData.population}</p>
+            `;
+        });
+
+        data.films.forEach(filmUrl => {
+            fetch(filmUrl)
+              .then(res => res.json())
+              .then(filmData => {
+                const filmElement = document.getElementById(`films`);
+                filmElement.innerHTML += `
+                    <p>${filmData.title}</p>
+                    <p>Episode ID: ${filmData.episode_id}</p>
+                    <p>Opening Crawl: ${filmData.opening_crawl}</p>
+                    <p>Director: ${filmData.director}</p>
+                    <p>Producer: ${filmData.producer}</p>
+                    <p>Release Date: ${filmData.release_date}</p>
+                    <p>Created: ${filmData.created}</p>
+                    <p>Edited: ${filmData.edited}</p>
+                    <p>Url: ${filmData.url}</p>
+                    -----------------------------------------------------------------------------------------------------------------------------
+                `;
+            });
+        });
+
+        data.species.forEach(speciesUrl => {
+            fetch(speciesUrl)
+              .then(res => res.json())
+              .then(speciesData => {
+                const speciesElement = document.getElementById(`species`);
+                speciesElement.innerHTML = ""
+                speciesElement.innerHTML += `
+                    <p>Name: ${speciesData.name}</p>
+                    <p>Classification: ${speciesData.classification}</p>
+                    <p>Designation: ${speciesData.designation}</p>
+                    <p>Average height: ${speciesData.average_height}</p>
+                    <p>Skin colors: ${speciesData.skin_colors}</p>
+                    <p>Eye colors: ${speciesData.eye_colors}</p>
+                    <p>Average lifespan: ${speciesData.average_lifespan}</p>
+                    <p>Homeworld: ${speciesData.homeworld}</p>
+                    <p>Language: ${speciesData.language}</p>
+                    <p>Created: ${speciesData.created}</p>
+                    <p>Edited: ${speciesData.edited}</p>
+                    <p>Url: ${speciesData.url}</p>
+                `;
+            });
+        });
+
+        data.vehicles.forEach(vehiclesUrl => {
+            fetch(vehiclesUrl)
+              .then(res => res.json())
+              .then(vehiclesData => {
+                const vehiclesElement = document.getElementById(`vehicles`);
+                vehiclesElement.innerHTML += `
+                    <p>Name: ${vehiclesData.name}</p>
+                    <p>Model: ${vehiclesData.model}</p>
+                    <p>Manufacturer: ${vehiclesData.manufacturer}</p>
+                    <p>Cost in credits: ${vehiclesData.cost_in_credits}</p>
+                    <p>Length: ${vehiclesData.length}</p>
+                    <p>Max atmosphering speed: ${vehiclesData.max_atmosphering_speed}</p>
+                    <p>Crew: ${vehiclesData.crew}</p>
+                    <p>Passengers: ${vehiclesData.passengers}</p>
+                    <p>Cargo capacity: ${vehiclesData.cargo_capacity}</p>
+                    <p>Consumables: ${vehiclesData.consumables}</p>
+                    <p>Vehicle class: ${vehiclesData.vehicle_class}</p>
+                    <p>Created: ${vehiclesData.created}</p>
+                    <p>Edited: ${vehiclesData.edited}</p>
+                    <p>Url: ${vehiclesData.url}</p>
+                `;
+            });
+        });
     })
 }
