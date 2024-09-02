2f76b52f2708efd75b1eee08d8673bca4ea92bb5
JesusBustamante <jesusgustamante08@gmail.com>
2024-09-02 17:26:47 -0500

    feat: :sparkles: next and previous buttons - audio

--- Dia9/script.js
+++ Dia9/script.js
@@ -2,29 +2,49 @@ document.getElementById(`inp`).addEventListener(`input`, GetDataFromAPI)
 
 function GetDataFromAPI() {
     var id = document.getElementById(`inp`).value;
+    idActual = id
     let url = `https://pokeapi.co/api/v2/pokemon/`+id;
     readid(url)
 };
 
 function readid(url) {
+
     fetch(url)
     .then (res => res.json())
     .then (data => {
         first = data.sprites.other.showdown.front_default
+        let song = data.cries.legacy
+        
 
         document.getElementById(`screen`).innerHTML = ""
         document.getElementById(`screen`).innerHTML += `
-            <img src ="${first}">
+        <img src ="${first}">
         `
         document.getElementById(`screens`).innerHTML = ""
         document.getElementById(`screens`).innerHTML += `
-        <p>-${data.name}</p>
+        <p>${data.name}</p>
         `
 
         document.getElementById(`screenss`).innerHTML = ""
         document.getElementById(`screenss`).innerHTML += `
-        <p>${data.id}</p>
+        <p>${data.id}-</p>
         `
+
+        audio = new Audio(song);
+        audio.play();
 
     })
 }
+
+
+document.getElementById(`botonp`).addEventListener(`click`, () => {
+    idActual--;
+    let url = `https://pokeapi.co/api/v2/pokemon/`+idActual;
+    readid(url);
+});
+
+document.getElementById(`botonn`).addEventListener(`click`, () => {
+    idActual++;
+    let url = `https://pokeapi.co/api/v2/pokemon/`+idActual;
+    readid(url);
+});
