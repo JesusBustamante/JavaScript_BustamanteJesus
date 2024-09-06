83c249db515f3ba277890936afe87fac193d40bd
JesusBustamante <jesusgustamante08@gmail.com>
2024-09-05 22:30:11 -0500

    feat: :sparkles: creation of show and hide information functions

--- Dia11/script.js
+++ Dia11/script.js
@@ -2,14 +2,73 @@ fetch("https://randomuser.me/api/")
 .then (res => res.json())
 .then (daticos => {
 
-    console.log(daticos.results[0].name)
     document.getElementById("perfil").innerHTML += `
         <image src = "${daticos.results[0].picture.large}">
     `
 
-    document.getElementById("container").innerHTML += `
-        <p id = "nombre">Hi, My name is</p>
-        <p id = name>${daticos.results[0].name.first}</p>
-    `
-    
-})
+    const infoContainer = document.getElementById("output");
+    let infoActual = null;
+
+    function mostrarInfo(info) {
+
+        if (infoActual) {
+            infoActual.style.display = "none";
+        }
+        infoContainer.innerHTML = info;
+        infoContainer.style.display = "block";
+    }
+
+    function ocultarInfo() {
+        if (infoActual) {
+            infoActual.style.display = "none";
+        }
+    }
+
+    document.getElementById("imagenes").addEventListener("mouseover", () => {
+        mostrarInfo(`
+            <p id = "info">Hi, My name is</p>
+            <p id = "infoo">${daticos.results[0].name.first}</p>
+        `);
+    });
+    document.getElementById("imagenes").addEventListener("mouseout", ocultarInfo);
+
+    document.getElementById("imagenes2").addEventListener("mouseover", () => {
+        mostrarInfo(`
+            <p id = "info">My email address is</p>
+            <p id = "infoo">${daticos.results[0].email}</p>
+        `);
+    });
+    document.getElementById("imagenes2").addEventListener("mouseout", ocultarInfo);
+
+    document.getElementById("imagenes3").addEventListener("mouseover", () => {
+        mostrarInfo(`
+            <p id = "info">My birthday is</p>
+            <p id = "infoo">${daticos.results[0].dob.date}</p>
+        `);
+    });
+    document.getElementById("imagenes3").addEventListener("mouseout", ocultarInfo);
+
+    document.getElementById("imagenes4").addEventListener("mouseover", () => {
+        mostrarInfo(`
+            <p id = "info">My address is</p>
+            <p id = "infoo">${daticos.results[0].location.street.number}<span>  ${daticos.results[0].location.street.name}</span></p>
+        `);
+    });
+    document.getElementById("imagenes4").addEventListener("mouseout", ocultarInfo);
+
+    document.getElementById("imagenes5").addEventListener("mouseover", () => {
+        mostrarInfo(`
+            <p id = "info">My phone number is</p>
+            <p id = "infoo">${daticos.results[0].phone}</p>
+        `);
+    });
+    document.getElementById("imagenes5").addEventListener("mouseout", ocultarInfo);
+
+    document.getElementById("imagenes6").addEventListener("mouseover", () => {
+        mostrarInfo(`
+            <p id = "info">My password is</p>
+            <p id = "infoo">${daticos.results[0].login.password}</p>
+        `);
+    });
+    document.getElementById("imagenes6").addEventListener("mouseout", ocultarInfo);
+});
