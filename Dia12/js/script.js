9d5356f23f0e8b3a5ecaf27ab2cd7647d06517db
JesusBustamante <jesusgustamante08@gmail.com>
2024-09-09 12:54:34 -0500

    feat: :sparkles: create and delete functions

--- Dia12/js/script.js
+++ Dia12/js/script.js
@@ -4,15 +4,15 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const addTaskButton = document.getElementById(`addTaskButton`);
 
     async function fetchData() {
-        const res = await fetch("https://6674179975872d0e0a950e53.mockapi.io/todoList")
+        const res = await fetch("https://66df335bde4426916ee3de16.mockapi.io/Tarea")
         data = await res.json();
         return data;
     }
 
     function displayCapsula(capsula) {
-        datosContenedor.innerHTML = "";
+        datosContenedor.innerHTML = ``;
         capsula.forEach(cap => {
-            const capDiv = document.createElement(`div`);
+            const capDiv = document.createElement('div');
             if (cap.status === "ready") {
                 capDiv.classList.add(`capsulaNegativa`);
                 capDiv.innerHTML = `
@@ -26,11 +26,11 @@ document.addEventListener(`DOMContentLoaded`, () => {
                     <div class="botonesNegativo">
 
                         <div class="terminadoNegativo">
-                            <img src="./storage/img/pngwing.com (2).png" data-id="1" class="completado">
+                            <img src="./storage/img/pngwing.com (2).png" data-id="${cap.id}" class="completado">
                         </div>
 
                         <div class="eliminadoNegativo">
-                            <img src="./storage/img/pngwing.com (4).png" data-id="2" class="eliminado">
+                            <img src="./storage/img/pngwing.com (4).png" data-id="${cap.id}" class="eliminado">
                         </div>
 
                     </div>
@@ -53,11 +53,11 @@ document.addEventListener(`DOMContentLoaded`, () => {
                     <div class="botonesPositivo">
 
                         <div class="terminado">
-                            <img src="./storage/img/pngwing.com (2).png" data-id="1" class="completado">
+                            <img src="./storage/img/pngwing.com (2).png" data-id="${cap.id}" class="completado">
                         </div>
 
                         <div class="eliminado">
-                            <img src="./storage/img/pngwing.com (4).png" data-id="2" class="eliminado">
+                            <img src="./storage/img/pngwing.com (4).png" data-id="${cap.id}" class="eliminado">
                         </div>
 
                     </div>
@@ -69,10 +69,68 @@ document.addEventListener(`DOMContentLoaded`, () => {
 
             datosContenedor.appendChild(capDiv)
         });
+
+        document.querySelectorAll('.completado').forEach(button => {
+            button.addEventListener('click', botonCompletado);
+        })
+
+        document.querySelectorAll('.eliminado').forEach(button => {
+            button.addEventListener('click', botonEliminado);
+        })
     }
 
-    fetchData().then(data => {
-        console.log(data);
+    async function addNewTask() {
+        const task = taskInput.value;
+        if (task.trim() === "") {
+            return
+        }
+
+        await fetch(`https://66df335bde4426916ee3de16.mockapi.io/Tarea`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                task,
+                status: 'On hold'
+            })
+        });
+        taskInput.value = '';
+        const data = await fetchData();
         displayCapsula(data);
-    })
+    }
+
+    addTaskButton.addEventListener('click', addNewTask);
+
+    async function botonCompletado(event) {
+        const id = event.target.getAttribute('data-id');
+        await fetch(`https://66df335bde4426916ee3de16.mockapi.io/Tarea/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body:JSON.stringify({status: 'ready'})
+        });
+        const data = await fetchData();
+        displayCapsula(data)
+    }
+
+    async function botonEliminado(evento) {
+        const id = evento.target.getAttribute('data-id');
+        await fetch(`https://66df335bde4426916ee3de16.mockapi.io/Tarea/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type' : 'application/json',
+            },
+            body:JSON.stringify({status: 'ready'})
+        });
+        const data = await fetchData();
+        displayCapsula(data)
+    }
+
+    fetchData()
+        .then(data => {
+            console.log(data);
+            displayCapsula(data);
+        })
 })
